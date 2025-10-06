c186ba0c578ccdae58cc0b51dbfd37ca45da3fd6
Abhijit Menon-Sen <ams@toroid.org>
2025-10-06 16:30:51 +0530

    Add a --short single-line output mode
    To save space in logs and permit easy comparison across lines.

--- seog-status.py
+++ seog-status.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python3
 
+import sys
 import serial
-import datetime
-
-now = datetime.datetime.now()
-print(now)
-
-# Hardcoded to the device name that was assigned when I plugged in the
-# serial-to-USB cable.
-
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-ser.write(b'A')
-s = ser.readline()
-ser.close()
-
-print("<<< " + s.hex())
 
 # Here's the example/explanation from Statcon Energiaa technical support:
 #
@@ -48,131 +35,163 @@ print("<<< " + s.hex())
 # 36 Byte in Hex=6E               (System Over Load)
 # 37 Byte in Hex=6E               (System Switched)
 
-# 'R' means the inverter is switched on.
-#
-# 'I' means the inverter is switched off.
+def parse(s):
+    d = {}
 
-status = chr(s[0])
-if status == 'R':
-    status = 'R (running)'
-elif status == 'I':
-    status = 'I (inactive)'
-print(f"Inverter status: {status}")
+    try:
+        # 'R'unning or 'I'nactive
 
-# 'B' means the load is on the grid (bypassing the inverter).
-#
-# 'F' means the load is on battery (at least) and/or solar (unsure).
+        d['status'] = chr(s[0])
 
-bat_status = ''
-if len(s) >= 24:
-    bat_status = chr(s[25])
+        # 'B' means the load is on the grid (bypassing the inverter).
+        # 'F' means the load is on battery and/or solar.
 
-load = chr(s[1])
-if load == 'B':
-    load = 'B (grid)'
-elif load == 'F':
-    if bat_status == 'y':
-        load = 'F (battery)'
-    else:
-        load = 'F (solar)'
-print(f"Load on: {load}")
+        d['load'] = chr(s[1])
 
-# The AC voltage and current reported appear to be the inverter's output voltage
-# and current, i.e., correlated with the load on the inverter. If the load is on
-# the grid, bypassing the inverter, these values are close to 0.
+        # The AC voltage and current reported appear to be the inverter's output
+        # voltage and current, i.e., correlated with the load on the inverter.
+        # If the load is on the grid, bypassing the inverter, these values are
+        # close to 0.
 
-ac_voltage = int(s[2:4].hex(), 16) / 10
-print(f"AC voltage: {ac_voltage}V")
+        d['ac_voltage'] = int(s[2:4].hex(), 16) / 10
+        d['ac_current'] = int(s[4:6].hex(), 16) / 10
 
-ac_current = int(s[4:6].hex(), 16) / 10
-print(f"AC current: {ac_current}A")
+        # The current is reported as an absolute value, whether the battery is
+        # charging or discharging. We return a negative value if the "battery
+        # status" is 'y' (I expected "charging status" to be the field that
+        # determines the sign, but it's only "battery status" that I've seen
+        # vary so far.)
 
-bat_voltage = int(s[6:8].hex(), 16) / 10
-print(f"Battery voltage: {bat_voltage}V")
+        d['bat_voltage'] = int(s[6:8].hex(), 16) / 10
+        d['bat_current'] = int(s[8:10].hex(), 16) / 10
 
-# The current is reported as an absolute value, whether the battery is charging
-# or discharging. We interpret the value as negative if "battery status" is 'y'
-# (I expected "charging status" to be the field that determines the sign, but
-# it's only "battery status" that I've seen vary so far.)
+        if len(s) >= 24:
+            bat_status = chr(s[25])
+            if bat_status == 'y':
+                d['bat_current'] *= -1
 
-bat_current = int(s[8:10].hex(), 16) / 10
-if bat_status == 'y':
-    bat_current *= -1
-print(f"Battery current: {bat_current}A")
+        d['pv_voltage'] = int(s[10:12].hex(), 16) / 10
+        d['pv_current'] = int(s[12:14].hex(), 16) / 10
+        d['pv_power'] = int(s[14:16].hex(), 16) / 1000
 
-pv_voltage = int(s[10:12].hex(), 16) / 10
-print(f"PV voltage: {pv_voltage}V")
+        # I'm just guessing at the right scale for the total kWh field. (The
+        # values I've seen reported for total/month seem inconsistent, so I'm
+        # probably not decoding it correctly here.)
 
-pv_current = int(s[12:14].hex(), 16) / 10
-print(f"PV current: {pv_current}A")
+        d['pv_units'] = int(s[16:18].hex(), 16) / 1000
+        d['pv_total'] = int(s[18:22].hex(), 16) / 10_000_000
+        d['pv_month'] = int(s[22:24].hex(), 16) / 1000
 
-pv_power = int(s[14:16].hex(), 16) / 1000
-print(f"PV power: {pv_power}kW")
+        d['fault'] = chr(s[24])
+        d['bat_status'] = chr(s[25])
+        d['charging_status'] = chr(s[26])
 
-pv_units = int(s[16:18].hex(), 16) / 1000
-print(f"PV units today: {pv_units:.2f}kWh")
+        # I speculate that the following fields all toggle between 0x6E ('n')
+        # and 0x79 ('y'), but I don't know for sure. It does work that way for
+        # PV under-voltage and the inverter switch, at least.
 
-# I'm just guessing at the right scale for the total kWh field.
+        d['inverter_uv'] = chr(s[27])
+        d['inverter_ov'] = chr(s[28])
+        d['battery_uv'] = chr(s[29])
+        d['battery_ov'] = chr(s[30])
+        d['overheating'] = chr(s[31])
+        d['tcs_fail'] = chr(s[32])
+        d['pv_ov'] = chr(s[33])
+        d['pv_uv'] = chr(s[34])
+        d['overload'] = chr(s[35])
+        d['switched_off'] = chr(s[36])
 
-pv_total = int(s[18:22].hex(), 16) / 10_000_000
-print(f"PV units total: {pv_total:.2f}kWh (uncertain)")
+    except IndexError:
+        pass
 
-# The values I've seen reported for this field seem inconsistent, so I'm
-# probably not decoding it correctly here.
+    return d
 
-pv_month = int(s[22:24].hex(), 16) / 1000
-print(f"PV units this month: {pv_month}kWh (uncertain)")
+def print_verbose(s, d):
+    print("<<< " + s.hex())
 
-fault = chr(s[24])
-if fault != 'n':
-    print(f"System fault: {fault}")
+    status = d['status']
+    if status == 'R':
+        status = 'R (running)'
+    elif status == 'I':
+        status = 'I (inactive)'
+    print(f"Inverter status: {status}")
 
-bat_status = chr(s[25])
-print(f"Battery status: {bat_status}")
+    load = d['load']
+    if load == 'B':
+        load = 'B (grid)'
+    elif load == 'F':
+        if d.get('bat_status', 'n') == 'y':
+            load = 'F (battery)'
+        else:
+            load = 'F (solar)'
+    print(f"Load on: {load}")
 
-charging_status = chr(s[26])
-print(f"Charging status: {charging_status}")
+    print(f"AC voltage: {d['ac_voltage']}V")
+    print(f"AC current: {d['ac_current']}A")
+    print(f"Battery voltage: {d['bat_voltage']}V")
+    print(f"Battery current: {d['bat_current']}A")
+    print(f"PV voltage: {d['pv_voltage']}V")
+    print(f"PV current: {d['pv_current']}A")
+    print(f"PV power: {d['pv_power']}kW")
+    print(f"PV units today: {d['pv_units']:.2f}kWh")
+    print(f"PV units total: {d['pv_total']:.2f}kWh (uncertain)")
+    print(f"PV units this month: {d['pv_month']}kWh (uncertain)")
+    print(f"Battery status: {d['bat_status']}")
+    print(f"Charging status: {d['charging_status']}")
 
-# I speculate that the following fields all toggle between 0x6E ('n') and 0x79
-# ('y'), but I don't know for sure. It does work that way for PV under-voltage,
-# at least.
+    flags = {
+        'fault': "System fault",
+        'inverter_uv': "Inverter under voltage",
+        'inverter_ov': "Inverter over voltage",
+        'battery_uv': "Battery under voltage",
+        'battery_ov': "Battery over voltage",
+        'overheating': "System overheating",
+        'tcs_fail': "TCS fail",
+        'pv_ov': "PV over voltage",
+        'pv_uv': "PV under voltage",
+        'overload': "System overload",
+    }
 
-inverter_uv = chr(s[27])
-if inverter_uv != 'n':
-    print(f"Inverter under voltage: {inverter_uv}")
+    for f in flags.keys():
+        v = d.get(f, 'n')
+        if v != 'n':
+            print(f"{flags[f]}: {v}")
 
-inverter_ov = chr(s[28])
-if inverter_ov != 'n':
-    print(f"Inverter over voltage: {inverter_ov}")
+    if d.get('switched_off', 'n') == 'y':
+        print(f"Inverter switched off: y")
 
-battery_uv = chr(s[29])
-if battery_uv != 'n':
-    print(f"Battery under voltage: {battery_uv}")
+def print_short(s, d):
+    l = f"status={d['status']}/{d['load']}/{d['bat_status']}/{d['charging_status']}"
+    l += f", AC=({d['ac_voltage']:05.1f}V; {d['ac_current']:04.1f}A)"
+    l += f", BAT=({d['bat_voltage']:04.1f}V; {d['bat_current']:05.1f}A)"
+    l += f", PV=({d['pv_voltage']:05.1f}V; {d['pv_current']:04.1f}A; {d['pv_power']:05.3}kW; {d['pv_units']:05.2f}kWh)"
 
-battery_ov = chr(s[30])
-if battery_ov != 'n':
-    print(f"Battery over voltage: {battery_ov}")
+    flags = [
+        'fault', 'inverter_uv', 'inverter_ov', 'battery_uv', 'battery_ov',
+        'overheating', 'tcs_fail', 'pv_ov', 'pv_uv', 'overload'
+    ]
+    for f in flags:
+        v = d.get(f, 'n')
+        if v != 'n':
+            l += f", {f}={v}"
 
-overheating = chr(s[31])
-if overheating != 'n':
-    print(f"System overheating: {overheating}")
+    if d.get('switched_off', 'n') == 'y':
+        l += f", switch=off"
 
-tcs_fail = chr(s[32])
-if tcs_fail != 'n':
-    print(f"TCS fail: {tcs_fail}")
+    print(l)
 
-pv_ov = chr(s[33])
-if pv_ov != 'n':
-    print(f"PV over voltage: {pv_ov}")
+if __name__ == '__main__':
+    # Hardcoded to the device name that was assigned when I plugged in the
+    # serial-to-USB cable.
 
-pv_uv = chr(s[34])
-if pv_uv != 'n':
-    print(f"PV under voltage: {pv_uv}")
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser.write(b'A')
+    s = ser.readline()
+    ser.close()
 
-overload = chr(s[35])
-if overload != 'n':
-    print(f"System overload: {overload}")
+    data = parse(s)
 
-switched = chr(s[36])
-if switched != 'n':
-    print(f"Inverter switched off: {switched}")
+    if len(sys.argv) == 1:
+        print_verbose(s, data)
+    elif sys.argv[1] == '--short':
+        print_short(s, data)
